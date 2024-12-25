29d12c837e4dc0cb93c939cb046b0875bc2be229
0721-Day <leng8768@gmail.com>
2024-12-26 02:40:05 +0800

    添加通配符支持

--- src/EventEmitter.ts
+++ src/EventEmitter.ts
@@ -7,19 +7,42 @@ export default class EventEmitter<EventKeyValues> {
     ) => void)[];
   } = {};
 
+  private _wildcard: ((
+    data: EventKeyValues[keyof EventKeyValues] & {
+      eventTag: keyof EventKeyValues;
+    }
+  ) => void)[] = [];
+
   public on<EventKey extends keyof EventKeyValues>(
     events: EventKey | EventKey[],
     handler: (data: EventKeyValues[EventKey] & { eventTag: EventKey }) => void
   ): () => void {
-
     const eventArray = Array.isArray(events) ? events : [events];
-    
+
     eventArray.forEach((event) => {
       if (!this._events[event]) this._events[event] = [];
       this._events[event]!.push(handler);
     });
 
     return () => eventArray.forEach((event) => this.off(event, handler));
+  }
+
+  public onAll<EventKey extends keyof EventKeyValues>(
+    handler: (data: EventKeyValues[EventKey] & { eventTag: EventKey }) => void
+  ) {
+    this._wildcard.push(handler);
+    return () => this.offAll(handler);
+  }
+
+  public offAll(
+    handler: (
+      data: EventKeyValues[keyof EventKeyValues] & {
+        eventTag: keyof EventKeyValues;
+      }
+    ) => void
+  ): void {
+    const index = this._wildcard.indexOf(handler);
+    if (index !== -1) this._wildcard.splice(index, 1);
   }
 
   public off<EventKey extends keyof EventKeyValues>(
@@ -41,5 +64,8 @@ export default class EventEmitter<EventKeyValues> {
     if (handlers) {
       handlers.forEach((handler) => handler({ ...data, eventTag: event }));
     }
+
+    // 触发通配符事件的处理函数
+    this._wildcard.forEach((handler) => handler({ ...data, eventTag: event }));
   }
 }
